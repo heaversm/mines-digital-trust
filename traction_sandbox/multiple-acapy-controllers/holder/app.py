6b5f6579568fc1203b01a00c8cad6fc923b4f34a
Jason Sy <jasyrotuck@gmail.com>
2021-11-18 15:12:14 -0800

    first attemp. it worked
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

---
+++ traction_sandbox/multiple-acapy-controllers/holder/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify, abort, request, make_response, current_app
+import logging 
+
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+
+TOPICS = ['proof-request']
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/log/topic/<topic>/", methods=['POST'])
+def acapy_event_handler(topic):
+    app.logger.info(f'topic={topic}')
+    app.logger.debug(request)
+
+    if topic in TOPICS:
+        app.logger.info(f"HANDLE {topic} event")
+    else:
+        app.logger.info(f"IGNORE {topic} event")
+
+    return make_response(jsonify({'success': True}), 200)
