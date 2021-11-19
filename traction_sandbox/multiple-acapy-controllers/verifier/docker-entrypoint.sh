3eb11373f33f515d091cb88a8f8e070889cfb729
Jason Sy <jasyrotuck@gmail.com>
2021-11-19 15:58:42 -0800

    do not run app if upgrade failes
    Signed-off-by: Jason Sy <jasyrotuck@gmail.com>

--- traction_sandbox/multiple-acapy-controllers/verifier/docker-entrypoint.sh
+++ traction_sandbox/multiple-acapy-controllers/verifier/docker-entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 python3 -m flask db upgrade
-python3 -m flask run --host=0.0.0.0
+if [ $? == 0 ]; then
+    python3 -m flask run --host=0.0.0.0
+fi
+echo 'Alembic db upgrade failed...'
+exit(1)
